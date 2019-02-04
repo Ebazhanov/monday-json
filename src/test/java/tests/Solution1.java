a1cb2627730fbd08fdcd322b963e5e93f226c3d7
Evgenii Bazhanov <evgenii.bazhanov@smava.de>
2019-02-04 23:38:27 +0100

    upd

---
+++ src/test/java/tests/Solution1.java
@@ -0,0 +1,40 @@
+package tests;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class Solution1 {
+
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+    public static void main(String[] args) {
+        //TO JSON
+        Address address = new Address( "Winterallee", 3);
+        String json = GSON.toJson(address);
+        System.out.println(json);
+
+        //FROM JSON
+        Address address1 = GSON.fromJson(json, Address.class);
+        System.out.println(address1.getStreet());
+        System.out.println(address1.getHouseNumber());
+    }
+
+    static class Address {
+        private String street;
+        private Integer houseNumber;
+
+        Address(String street, Integer houseNumber) {
+            this.street = street;
+            this.houseNumber = houseNumber;
+        }
+
+        String getStreet() {
+            return street;
+        }
+
+        Integer getHouseNumber() {
+            return houseNumber;
+        }
+    }
+
+}
