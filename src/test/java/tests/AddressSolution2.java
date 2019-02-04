b0e06b4fc1963e19dcb87c21d3ee93faa92704c5
Evgenii Bazhanov <evgenii.bazhanov@smava.de>
2019-02-05 00:59:49 +0100

    added one more tests

--- src/test/java/tests/AddressSolution2.java
+++ src/test/java/tests/AddressSolution2.java
@@ -4,6 +4,8 @@ package tests;
 import com.bluelinelabs.logansquare.LoganSquare;
 import com.bluelinelabs.logansquare.annotation.JsonField;
 import com.bluelinelabs.logansquare.annotation.JsonObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import java.io.IOException;
 
@@ -12,17 +14,58 @@ public class AddressSolution2 {
     @JsonField(name = "street")
     public String street;
     @JsonField(name = "housenumber")
-    public int housenumber;
+    public String housenumber;
 
-    public void say() {
-        System.out.println();
-        System.out.println("I am leaving on the street name " + street + " and  my house number is " + housenumber + " in Berlin");
+    public String getStreet() {
+        return street;
     }
 
-    public static void main(String[] args) throws IOException {
+    public String getHousenumber() {
+        return housenumber;
+    }
+
+    // 1. Write a simple program that does the task for the most simple cases, e.g.
+
+    @Test
+    public void firstTest() throws IOException {
         String jsonString = "{\"street\": \"Winterallee\", \"housenumber\": \"3\"}";
-        AddressSolution2 addressSolution2 = LoganSquare.parse(jsonString, AddressSolution2.class);
-        addressSolution2.say();
+        AddressSolution2 json = LoganSquare.parse(jsonString, AddressSolution2.class);
+        Assert.assertEquals("Winterallee 3", json.getStreet() + " " + json.getHousenumber());
     }
+
+    @Test
+    public void secondTest() throws IOException {
+        String jsonString = "{\"street\": \"Musterstrasse\", \"housenumber\": \"45\"}";
+        AddressSolution2 json = LoganSquare.parse(jsonString, AddressSolution2.class);
+        Assert.assertEquals("Musterstrasse 45", json.getStreet() + " " + json.getHousenumber());
+    }
+
+    @Test
+    public void thirdTest() throws IOException {
+        String jsonString = "{\"street\": \"Blaufeldweg\", \"housenumber\": \"123B\"}";
+        AddressSolution2 json = LoganSquare.parse(jsonString, AddressSolution2.class);
+        Assert.assertEquals("Blaufeldweg 123B", json.getStreet() + " " + json.getHousenumber());
+    }
+
+    // 2. Consider more complicated cases
+
+    @Test
+    public void fourthTest() throws IOException {
+        String jsonString = "{\"street\": \"Am Bächle\", \"housenumber\": \"23\"}";
+        AddressSolution2 json = LoganSquare.parse(jsonString, AddressSolution2.class);
+        Assert.assertEquals("Am Bächle 23", json.getStreet() + " " + json.getHousenumber());
+    }
+
+    @Test
+    public void fifthTest() throws IOException {
+        String jsonString = "{\"street\": \"Auf der Vogelwiese\", \"housenumber\": \"23 b\"}";
+        AddressSolution2 json = LoganSquare.parse(jsonString, AddressSolution2.class);
+        Assert.assertEquals("Auf der Vogelwiese 23 b", json.getStreet() + " " + json.getHousenumber());
+    }
+
+    // 3. BONUS: Consider other countries (complex cases)
+
+    //TODO
+
 
 }
