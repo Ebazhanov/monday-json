ede2efe2ef5876cb5c79b3f0d5a06cab33e32a23
Evgenii Bazhanov <evgenii.bazhanov@smava.de>
2019-02-07 11:15:36 +0100

    upd

--- src/test/java/tests/Alex.java
+++ src/test/java/tests/Alex.java
@@ -10,8 +10,8 @@ public class Alex {
     @Test
     public void shouldHaveWinterallee() {
         final String input = "Winterallee 3";
-        int numberStarted = -1;
-        int returnedNumbers = findFirstNumberInString(input, numberStarted);
+        //int numberStarted = -1;
+        int returnedNumbers = findFirstNumberInString(input);
         final String streetName = input.substring(0, returnedNumbers - 1);
         final String houseNumber = input.substring(returnedNumbers);
         final Address json = new Address(streetName, houseNumber);
@@ -19,28 +19,16 @@ public class Alex {
         assertEquals(json.getHouseNumber(), "3");
     }
 
-    @Test
-    public void shouldHaveVogelwiese() {
-        final String input = "Auf der Vogelwiese 23 b";
-        int numberStarted = -1;
-        int returnedNumbers = findFirstNumberInString(input, numberStarted);
-        final String streetName = input.substring(0, returnedNumbers - 1);
-        final String houseNumber = input.substring(returnedNumbers);
-        final Address json = new Address(streetName, houseNumber);
-        assertEquals(json.getStreetName(), "Auf der Vogelwiese");
-        assertEquals(json.getHouseNumber(), "23 b");
-    }
-
-
-    private int findFirstNumberInString(String input, int numberStarted) {
+    private int findFirstNumberInString(String input) {
         for (int index = 0; index < input.length(); index++) {
             final char charAt = input.charAt(index);
             if (Character.isDigit(charAt)) {
-                numberStarted = index;
+                //int numberStarted = index;
                 //break;
+                return index;
             }
         }
-        return numberStarted;
+        return -1;
     }
 
 }
