679dc5e861e8105a16889865fc4f3f0619ac3d18
Evgenii Bazhanov <evgenii.bazhanov@smava.de>
2019-02-11 19:40:37 +0100

    upd

--- src/test/java/tests/MoreComplexTests.java
+++ src/test/java/tests/MoreComplexTests.java
@@ -10,59 +10,60 @@ public class MoreComplexTests {
 
     private final static Gson GSON = new Gson();
 
-    public static int findFirstNumberInString(String input) {
-        for (int index = 0; index < input.length(); index++) {
-            final char charAt = input.charAt(index);
-            if (Character.isDigit(charAt)) {
-                return index;
-            }
-        }
+    public static int findFirstNumberInStringNew(String input) {
+        //int inputNo = input.indexOf("No");
+        //int inputСomma = input.indexOf(",");
+
+        if (input.contains("No")) {
+            return input.indexOf("No");
+        } else if (input.contains(",")) {
+            return input.indexOf(",");
+        } /*else if (!(input.indexOf(",") | input.indexOzf("No")) {
+            System.out.println("НЕТУ номера и запятой");
+        }*/
+
         return -1;
     }
 
     public String parse(String input) {
-        //final int firstNumberIndex = findFirstNumberInString(input);
-
-
-        int inputNo = input.indexOf("No");
-        int inputСomma = input.indexOf(",");
-
-        System.out.println(inputNo);
-        if (inputNo > 0) {
-            return String.valueOf(inputNo);
-        } else if (inputСomma > 0) {
-            return String.valueOf(inputСomma);
-        } else if (inputNo < 0 | inputСomma < 0) {
-            System.out.println("НЕТУ номера и запятой");
-        }
-
-        final String streetName = input.substring(0, inputNo - 1);
-        final String houseNumber = input.substring(inputNo);
-
-
-/*        if (input.indexOf(",") != 0) {
-            System.out.println("ЗАПЯТАЯ");
-        }*/
-
+        final int firstNumberIndex = findFirstNumberInStringNew(input);
+        final String streetName = input.substring(0, firstNumberIndex-1);
+        final String houseNumber = input.substring(firstNumberIndex);
         final JsonObject jsonAddress = new JsonObject();
-
         jsonAddress.addProperty("street", streetName);
         jsonAddress.addProperty("housenumber", houseNumber);
         return GSON.toJson(jsonAddress);
     }
 
     @Test
-    public void shouldParseComplicatedAddress1() {
+    public void shouldParseOtherCountriesAddress1() {
+        final String input = "4, rue de la revolution";
+        final String expectedJson = "{\"street\":\"rue de la revolution\",\"housenumber\":\"4\"}";
+        final String actualJson = parse(input);
+        assertEquals(actualJson, expectedJson);
+    }
+
+    @Test
+    public void shouldParseOtherCountriesAddress2() {
+        final String input = "200 Broadway Av";
+        final String expectedJson = "{\"street\":\"Broadway Av\",\"housenumber\":\"200\"}";
+        final String actualJson = parse(input);
+        assertEquals(actualJson, expectedJson);
+    }
+
+    @Test
+    public void shouldParseOtherCountriesAddress3() {
+        final String input = "Calle Aduana, 29";
+        final String expectedJson = "{\"street\":\"Calle Aduana\",\"housenumber\":\"29\"}";
+        final String actualJson = parse(input);
+        assertEquals(actualJson, expectedJson);
+    }
+
+    @Test
+    public void shouldParseOtherCountriesAddress4() {
         final String input = "Calle 39 No 1540";
-        //int inputString = input.indexOf(",");
-
-/*        if (inputString) {
-            System.out.println(input.indexOf(","));
-        }*/
-
         final String expectedJson = "{\"street\":\"Calle 39\",\"housenumber\":\"No 1540\"}";
         final String actualJson = parse(input);
-
         assertEquals(actualJson, expectedJson);
     }
 
